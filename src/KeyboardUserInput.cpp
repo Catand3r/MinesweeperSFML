972d11ff76bc8165c9c807707ece251fcc5e7bd0
Paweł Ziemann <paweljar.ziemann@gmail.com>
2024-10-25 19:58:04 +0200

    Update cmake

--- src/KeyboardUserInput.cpp
+++ src/KeyboardUserInput.cpp
@@ -3,34 +3,38 @@
 
 Action KeyboardUserInput::PollEvent()
 {
-	Action action;
-	std::cout << "Wybierz wspolrzedna x: ";
-	int x;
-	std::cin >> x;
-	std::cout << "Wybierz wspolrzedna y: ";
-	int y;
-	std::cin >> y;
-	action.actionType_ = ActionType::CheckCell;
-	action.playerPos_ = { x, y };
-	return action;
+    Action action;
+    std::cout << "Wybierz wspolrzedna x: ";
+    int x;
+    std::cin >> x;
+    std::cout << "Wybierz wspolrzedna y: ";
+    int y;
+    std::cin >> y;
+    action.actionType_ = ActionType::CheckCell;
+    action.playerPos_ = {x, y};
+    return action;
 }
 
-bool KeyboardUserInput::Init(const Cells&, const MinePositions&, const int&)
+bool KeyboardUserInput::Init(const Cells &, const MinePositions &, const int &)
 {
-	return true;
+    return true;
 }
 
 void KeyboardUserInput::OnResultEmpty(int x, int y, int minesAroundCell)
 {
-	std::cout << "Na wspolrzednych: " << x << " i " << y << " jest pusto, a wokol sa " << minesAroundCell << " miny. Grasz dalej\n";
+    std::cout << "Na wspolrzednych: " << x << " i " << y << " jest pusto, a wokol sa " << minesAroundCell
+              << " miny. Grasz dalej\n";
 }
 
 void KeyboardUserInput::OnResultMine(int x, int y)
 {
-	std::cout << "Na wspolrzednych: " << x << " i " << y << " jest mina. Przegrywasz";
+    std::cout << "Na wspolrzednych: " << x << " i " << y << " jest mina. Przegrywasz";
 }
 
 void KeyboardUserInput::OnMarkCell(int x, int y, int)
 {
+}
 
+void KeyboardUserInput::OnGameWon()
+{
 }
